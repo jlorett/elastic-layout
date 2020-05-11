7f837e9c850c652e18ce84dcafff6fd49bf2c9be
jal <joshlorett@gmail.com>
2020-05-11 15:02:36 -0400

    Add float range to communicate alpha limit on BackdropFader

--- sample/src/main/java/com/joshualorett/elasticlayout/sample/BackdropFader.kt
+++ sample/src/main/java/com/joshualorett/elasticlayout/sample/BackdropFader.kt
@@ -1,9 +1,11 @@
 package com.joshualorett.elasticlayout.sample
 
+import androidx.annotation.FloatRange
+
 /**
  * Fades backdrop alpha.
  * Created by Joshua on 5/10/2020.
  */
 interface BackdropFader {
-    fun fade(alpha: Float)
+    fun fade(@FloatRange(from=0.0, to=1.0) alpha: Float)
 }
